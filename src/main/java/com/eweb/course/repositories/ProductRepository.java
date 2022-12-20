9d4bd28358517bdcecc115f5f6245af340c8d7ee
caio pivetta <caiopivetta6@gmail.com>
2022-12-20 11:19:14 +0100

    Products.Entity

---
+++ src/main/java/com/eweb/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.eweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eweb.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	
+}
