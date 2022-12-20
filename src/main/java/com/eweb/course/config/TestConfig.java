4bcf37885d3ed49b4e0a7143732c5724fafa27b7
caio pivetta <caiopivetta6@gmail.com>
2022-12-20 15:26:40 +0100

    Products.Entity

--- src/main/java/com/eweb/course/config/TestConfig.java
+++ src/main/java/com/eweb/course/config/TestConfig.java
@@ -40,11 +40,14 @@ public class TestConfig implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 		
+		
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		//productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
@@ -56,6 +59,14 @@ public class TestConfig implements CommandLineRunner {
 		Order o1 = new Order(null, Instant.parse("2023-10-02T19:54:07Z"), OrderStatus.PAID,u1);
 		Order o2 = new Order(null, Instant.parse("2021-10-05T19:54:07Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2024-12-02T19:54:07Z"),OrderStatus.DELIVERED,u1);
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p3.getCategories().add(cat1);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
