fb93bb0cb18d70d50c216c77bceba5c5cfdf7d5e
caio pivetta <caiopivetta6@gmail.com>
2022-12-24 17:04:03 +0100

    Exception Handling - Update

--- src/main/java/com/eweb/course/services/UserService.java
+++ src/main/java/com/eweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.eweb.course.repositories.UserRepository;
 import com.eweb.course.services.exceptions.DatabaseException;
 import com.eweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -48,10 +50,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
-		
 		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		
 	}
 	
